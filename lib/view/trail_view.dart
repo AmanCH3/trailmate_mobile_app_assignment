255097e61fbf0071e7afd129f1ddf02c3c75133c
AmanCH3 <armanachaudhary53@gmail.com>
2025-06-02 18:28:58 +0545

    feat : searchAnchor in the trail

--- lib/view/trail_view.dart
+++ lib/view/trail_view.dart
@@ -8,8 +8,79 @@ class TrailView extends StatefulWidget {
 }
 
 class _TrailViewState extends State<TrailView> {
+  final SearchController _searchController = SearchController();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: const Text("Trails "));
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: SearchAnchor(
+              searchController: _searchController,
+              builder: (BuildContext context, SearchController controller) {
+                return SearchBar(
+                  controller: controller,
+                  hintText: 'Search trails...',
+                  padding: const WidgetStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+                  // onTap: controller.openView,
+                  onChanged: (context) => controller.openView(),
+                  leading: const Icon(Icons.search),
+                );
+              },
+              suggestionsBuilder: (
+                BuildContext context,
+                SearchController controller,
+              ) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'Trail $index';
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      setState(() {
+                        controller.closeView(item);
+                      });
+                    },
+                  );
+                });
+              },
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.green,
+            ),
+
+            child: IconButton(
+              icon: const Icon(Icons.filter_alt_outlined),
+              tooltip: 'Filter Trails',
+
+              onPressed: () {
+                // Add filter functionality here
+                showDialog(
+                  context: context,
+                  builder:
+                      (context) => AlertDialog(
+                        title: const Text('Filter Options'),
+                        content: const Text('Filter logic goes here...'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text('Close'),
+                          ),
+                        ],
+                      ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
