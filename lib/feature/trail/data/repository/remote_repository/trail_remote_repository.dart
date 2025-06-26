0a2696be705127650f0358bcd62995412d7502f4
amanch3 <armanachaudhary53@gmail.com>
2025-06-26 08:43:00 +0545

    feat : trail remote repository

---
+++ lib/feature/trail/data/repository/remote_repository/trail_remote_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:trailmate_mobile_app_assignment/core/error/failure.dart';
+import 'package:trailmate_mobile_app_assignment/feature/trail/data/data_source/remote_data_source/trail_remote_datasource.dart';
+import 'package:trailmate_mobile_app_assignment/feature/trail/domain/entity/trail_entity.dart';
+import 'package:trailmate_mobile_app_assignment/feature/trail/domain/repository/trail_repository.dart';
+
+class TrailRemoteRepository implements ITrailRepository {
+  final TrailRemoteDataSource trailRemoteDataSource;
+
+  TrailRemoteRepository({required this.trailRemoteDataSource});
+
+  @override
+  Future<Either<Failure, List<TrailEnitiy>>> getTrails() async {
+    try {
+      final trail = await trailRemoteDataSource.getTrails();
+      return Right(trail);
+    } catch (e) {
+      return Left(ApiFailure(statusCode: 500, message: 'Server error'));
+    }
+  }
+}
